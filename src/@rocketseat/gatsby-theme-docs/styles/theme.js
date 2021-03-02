734da8c0c824a312cc00b739e7eece700d481118
fs5013-furi-sutao <fs5013.furi.futao@gmail.com>
2021-03-02 20:17:12 +0900

    Add article

---
+++ src/@rocketseat/gatsby-theme-docs/styles/theme.js
@@ -0,0 +1,12 @@
+export default {
+  colors: {
+    // primary: '#8257E6',
+    primary: '#ff9933',
+    background: '#FFFFFF',
+    shape: `#F2F2FA`,
+    title: `#3D3D4D`,
+    text: `#6C6C80`,
+    // link: `#318ce7`,#446ccf
+    link: `#5218fa`,
+  },
+}
